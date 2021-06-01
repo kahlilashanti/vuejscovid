de26b90ccfb3c2217ace4fbf7d628300fa256dc8
Kahlil Ashanti <kahlil.ashanti@gmail.com>
2021-06-01 13:16:43 -0700

    added fetching data

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,8 @@
 <template>
   <header class="text-center bg-blue-800 text-white p-4 mb-10">
-    <div class="text-3xl md:text-5xl font-bold mb-3">COVID Tracker</div>
+    <div class="text-3xl md:text-5xl font-bold mb-3">
+      <i class="fa fa-viruses"></i>COVID Tracker
+    </div>
   </header>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,41 @@
 <template>
-  <div class="home">This works</div>
+  <main v-if="!loading">Show Data</main>
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Latest Data</div>
+    <img :src="loadingImage" class="w-24 m-auto" alt="" />
+  </main>
 </template>
 
 <script>
 export default {
   name: "Home",
   components: {},
+  data() {
+    return {
+      loading: true,
+      title: "Global",
+      dataDate: "",
+      stats: {},
+      countries: [],
+      loadingImage: require("../assets/big-head.gif"),
+    };
+  },
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch("https://api.covid19api.com/summary");
+      const data = await res.json();
+      return data;
+    },
+  },
+  async created() {
+    // console.log(123);
+    const data = await this.fetchCovidData();
+    // console.log(data);
+    // assign values to the empty elements in lines 13-15
+    this.dataDate = data.Date;
+    this.stats = data.Global;
+    this.countries = data.Countries;
+    this.loading = false;
+  },
 };
 </script>
