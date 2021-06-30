1c6e64ffd560382df5aebdad8ec03907c58bdc7f
Kahlil Ashanti <kahlil.ashanti@gmail.com>
2021-06-30 15:33:10 -0700

    updated country picker

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <main v-if="!loading">
     <DataTitle :text="title" :dataDate="dataDate" />
     <DataBoxes :stats="stats" />
+    <CountrySelect @get-country="getCountryData" :countries="countries" />
   </main>
   <main class="flex flex-col align-center justify-center text-center" v-else>
     <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Latest Data</div>
@@ -14,12 +15,14 @@
 <script>
 import DataTitle from "@/components/DataTitle";
 import DataBoxes from "@/components/DataBoxes";
+import CountrySelect from "@/components/CountrySelect";
 
 export default {
   name: "Home",
   components: {
     DataTitle,
     DataBoxes,
+    CountrySelect,
   },
   data() {
     return {
@@ -36,6 +39,11 @@ export default {
       const res = await fetch("https://api.covid19api.com/summary");
       const data = await res.json();
       return data;
+    },
+
+    getCountryData(country) {
+      this.stats = country;
+      this.title = country.Country;
     },
   },
   async created() {
