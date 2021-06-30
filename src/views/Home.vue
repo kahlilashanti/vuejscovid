acc786df2e4d5377cf540f2b5558738c516c957c
Kahlil Ashanti <kahlil.ashanti@gmail.com>
2021-06-30 11:45:27 -0700

    added cases and deaths

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,26 @@
 <template>
-  <main v-if="!loading">Show Data</main>
+  <main v-if="!loading">
+    <DataTitle :text="title" :dataDate="dataDate" />
+    <DataBoxes :stats="stats" />
+  </main>
   <main class="flex flex-col align-center justify-center text-center" v-else>
     <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Latest Data</div>
     <img :src="loadingImage" class="w-24 m-auto" alt="" />
   </main>
 </template>
 
+
+
 <script>
+import DataTitle from "@/components/DataTitle";
+import DataBoxes from "@/components/DataBoxes";
+
 export default {
   name: "Home",
-  components: {},
+  components: {
+    DataTitle,
+    DataBoxes,
+  },
   data() {
     return {
       loading: true,
